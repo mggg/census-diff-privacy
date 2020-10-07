b9c5b4a7b6af27579fb1a6831885d93ca7869470
Bhushan Suwal <bhushan_suwal@live.com>
2020-10-06 23:18:48 -0400

    district variance works

--- toydown/toydown.py
+++ toydown/toydown.py
@@ -2,53 +2,97 @@ from treelib import Node, Tree
 from errors import SumError
 import numpy as np
 import math
-
-class GeoUnit(object):
-    """ This class stores the data inside each Node in the tree.
-    """
-    def __init__(self, name, parent, unnoised_pop, identifier=None):
-        """ Args:
-                name         : Str, Name of the Node
-                parent       : Str, The identifier of the parent of the Node
-                unnoised_pop : Int, Unnoised population
-                identifier   : Str, A string that is unique to the Node in the entire Tree
-        """
-        self.name = name
-        self.unnoised_pop = unnoised_pop
-        self.parent = parent
-        if identifier:
-            self.identifier = identifier
-        else:
-            self.identifier = name
+import geopandas as gpd
+# from gerrychain import Graph, Partition
 
 class ToyDown(Tree):
-    def __init__(self, hierarchy, eps_budget, eps_splits):
-        """ Initializes the Tree and populates it.
-
-            hierarchy  : List of tuples of the form (name, branching, population) sorted
-                         in decreasing order of geographic nesting for eg.
-                         [("Country", 1, 810), ("State", 3, 270), ("County", 3, 90), ("Dist", 3, 30)]
-                         has 1 Country of population 810, 3 States of population 270 each,
-                         3 Counties in each State of population 90 each and 3 Dists in each County of
-                         population 30 each.
-            eps_budget : Float, Epsilon budget across all levels
-            eps_splits : List denoting the % of splits in epsilon value by level
-                         eg. if the hierarchy is [Country, State, County, District] then
-                         the eps_splits could look like [0, 0.33, 0.33, 0.34]
+    def __init__(self, filename, geoid_col, pop_col, parental_offsets=[3, 1, 6, 3, 2]):
+        """
         """
         super(ToyDown, self).__init__()
-        self.eps_budget = eps_budget
 
-        # create nodes and populate the tree
-        geounits = []
-        self.add_geounits_at_level_to_list(hierarchy, 0, None, geounits)
+        # self.graph = Graph.from_file(filename)
+        self.gdf = gpd.read_file(filename)
+        self.leaf_id = geoid_col
+        self.pop_col = pop_col
+        self.total_pop = self.gdf[self.pop_col].sum()
+        self.levels = len(parental_offsets)
+
+        geounits = self.create_tree_geounits_from_leaves(parental_offsets)
         self.populate_tree(geounits)
-        # self.populate_tree(hierarchy)
+        self.add_levels_to_node(self.get_node(self.root), 0)
+
         self.print_unnoised_totaling_errors(self.get_node(self.root))
 
-        self.add_levels_to_node(self.get_node(self.root), 0)
-        self.eps_values = self.epsilon_values(eps_splits, eps_budget)
+    # def from_hierarchy(cls, hierarchy, eps_budget, eps_splits):
+    #     """ Initializes the Tree and populates it.
+    #
+    #         hierarchy  : List of tuples of the form (name, branching, population) sorted
+    #                      in decreasing order of geographic nesting for eg.
+    #                      [("Country", 1, 810), ("State", 3, 270), ("County", 3, 90), ("Dist", 3, 30)]
+    #                      has 1 Country of population 810, 3 States of population 270 each,
+    #                      3 Counties in each State of population 90 each and 3 Dists in each County of
+    #                      population 30 each.
+    #         eps_budget : Float, Epsilon budget across all levels
+    #         eps_splits : List denoting the % of splits in epsilon value by level
+    #                      eg. if the hierarchy is [Country, State, County, District] then
+    #                      the eps_splits could look like [0, 0.33, 0.33, 0.34]
+    #     """
+    #     super(ToyDown, self).__init__()
+    #
+    #     self.eps_budget = eps_budget
+    #     self.eps_values = self.epsilon_values(eps_splits, eps_budget)
+    #
+    #     # create nodes and populate the tree
+    #     geounits = []
+    #     self.add_geounits_at_level_to_list(hierarchy, 0, None, geounits)
+    #     self.populate_tree(geounits)
+    #     self.print_unnoised_totaling_errors(self.get_node(self.root))
+    #     self.add_levels_to_node(self.get_node(self.root), 0)
 
+    def create_tree_geounits_from_leaves(self, parental_offsets):
+        """
+        """
+        fliped_offsets = np.flip(parental_offsets)
+        ls = np.cumsum(fliped_offsets)
+
+        leafs = self.gdf[[self.leaf_id, self.pop_col]]
+        nodes = self.gdf[[self.leaf_id, self.pop_col]]
+
+        for i, offset in enumerate(np.cumsum(parental_offsets)[:-1]):
+            level_names = leafs[self.leaf_id].apply(lambda s: s[:-offset])
+            nodes = nodes.append(leafs.groupby(level_names).sum().reset_index(), ignore_index=True)
+
+        node_dict = nodes.set_index(self.leaf_id).to_dict(orient="index")
+
+        geounits = []
+        for k ,v in node_dict.items():
+            i, = np.where(ls == len(k))[0]
+            par = k[:-fliped_offsets[i]]
+            if par == "": par = None
+            geounits.insert(0, self.GeoUnit(k, par, v[self.pop_col]))
+
+        return geounits
+
+    class GeoUnit(object):
+        """ This class stores the data inside each Node in the tree.
+        """
+        def __init__(self, name, parent, unnoised_pop, identifier=None):
+            """ Args:
+                    name         : Str, Name of the Node
+                    parent       : Str, The identifier of the parent of the Node
+                    unnoised_pop : Int, Unnoised population
+                    identifier   : Str, A string that is unique to the Node in the entire Tree
+            """
+            self.name = name
+            self.unnoised_pop = unnoised_pop
+            self.parent = parent
+            self.identifier = identifier if identifier else name
+
+        def __repr__(self):
+            name = self.__class__.__name__
+            kwargs = [ "{}={}".format(k, v) for k, v in self.__dict__.items()]
+            return "%s(%s)" % (name, ", ".join(kwargs))
 
     def add_geounits_at_level_to_list(self, hierarchy, level, parent, all_units):
         """ Recursively create the list of geounits needed to build the tree as specified in the `hierarchy`.
@@ -70,13 +114,13 @@ class ToyDown(Tree):
         if level == 0:
             # build root
             name, branching, pop = hierarchy[level]
-            all_units.append(GeoUnit(name, None, pop))
+            all_units.append(self.GeoUnit(name, None, pop))
             self.add_geounits_at_level_to_list(hierarchy, level+1, name, all_units)
         else:
             name, branching, pop = hierarchy[level]
             for i in range(branching):
                 curr_name = parent + name + str(i+1)
-                all_units.append(GeoUnit(curr_name, parent, pop))
+                all_units.append(self.GeoUnit(curr_name, parent, pop))
                 self.add_geounits_at_level_to_list(hierarchy, level+1, curr_name, all_units)
 
     def epsilon_values(self, eps_splits, eps_budget):
@@ -220,3 +264,49 @@ class ToyDown(Tree):
 
             for child in self.children(node.identifier):
                 self.print_unnoised_totaling_errors(child)
+
+    def assign_district_tree_variance(self, district, eps=None, eps_splits=None, sensitivity=2):
+        """
+        """
+        self.assign_district_to_leaves(district)
+        root = self.get_node(self.root)
+        print(root)
+        self.assign_weights(root)
+
+        epsilons = np.ones(self.levels)*np.sqrt(2) if eps_splits == None else np.array(eps_splits)
+        if eps != None:
+            epsilons *= eps if eps_splits else eps*(1/np.sqrt(2))*(1/self.levels)
+
+        return self.district_variance(root, epsilons, sensitivity)
+
+    def assign_district_to_leaves(self, district):
+        """
+        """
+        for leaf, weight in district.items():
+            self.get_node(leaf).data.weight = weight
+
+    def assign_weights(self, node):
+        """
+        """
+        if not node.is_leaf():
+            children = self.children(node.identifier)
+            child_weights = [self.assign_weights(child) for child in children]
+            node.data.weight = np.mean(child_weights)
+        return node.data.weight
+
+    def district_variance(self, node, epsilons, sensitivity):
+        """
+        """
+        eps_k = epsilons[0]
+        
+        if node.is_leaf():
+            child_vars = []
+        else:
+            children = self.children(node.identifier)
+            child_vars = [self.district_variance(child, epsilons[1:], sensitivity) for child in children]
+
+        if node.data.parent == None:
+            return node.data.weight**2 * 2*(sensitivity/eps_k)**2 + sum(child_vars)
+        else:
+            par_weight = self.get_node(node.data.parent).data.weight
+            return (node.data.weight - par_weight)**2 * 2*(sensitivity/eps_k)**2 + sum(child_vars)
