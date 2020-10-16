d60908292dd03b9068ee49c055d9427d848453e2
JN <jenniniels.matthews@gmail.com>
2020-10-16 16:33:02 -0400

    Add rasterization to ER plots.

--- multi_attribute_toydown/ER_noise.py
+++ multi_attribute_toydown/ER_noise.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import LinearRegression
 
 
 ## Compare ER with ToyDown/TopDown noised data
-def plot_er_graph(data, cand, race, elect, elect_col, tot_vote, eps, split, filt=True, n_samps=32, 
+def plot_er_graph(data, cand, race, elect, cand_perc_col, tot_vote, eps, split, filt=True, n_samps=32, 
                   ax=None, title=True, plot_cvap=False, weight=False):
     
     df = data.query("epsilon == @eps & split == @split")
@@ -18,7 +18,7 @@ def plot_er_graph(data, cand, race, elect, elect_col, tot_vote, eps, split, filt
     num_precints = df.shape[0]
     xs = np.reshape(np.linspace(0, 1, 100), (100,1))
     perc_race = np.reshape(df["{}_pct".format(race)].fillna(0).values, (num_precints, 1))
-    perc_cand = df["{}{}".format(cand,elect_col)].fillna(0) 
+    perc_cand = df["{}".format(cand_perc_col)].fillna(0) 
     weights = df[tot_vote].values if weight else None
     line = LinearRegression().fit(perc_race, perc_cand, weights)
 
@@ -37,10 +37,10 @@ def plot_er_graph(data, cand, race, elect, elect_col, tot_vote, eps, split, filt
             
         line_noised = LinearRegression().fit(perc_race_noised, perc_cand, weights)
         ms[i] = line_noised.coef_[0]
-        ax.plot(perc_race_noised, perc_cand, '.', color="lightcoral")
+        ax.plot(perc_race_noised, perc_cand, '.', color="lightcoral", rasterized=True)
         ax.plot(xs, line_noised.predict(xs), '-', color="red")
     
-    ax.plot(perc_race, perc_cand, '.', color="b")
+    ax.plot(perc_race, perc_cand, '.', color="b", rasterized=True)
     ax.plot(xs, line.predict(xs), '-', color="b", 
              label="m: {}".format(round(line.coef_[0], 3)))
     ax.plot([], [], '-', color="r", 
@@ -52,7 +52,7 @@ def plot_er_graph(data, cand, race, elect, elect_col, tot_vote, eps, split, filt
     return ax
 
 ## Plot grid of epsilon values vs. splits for ToyDown/TopDown ER plots
-def plot_elect_grid(epsilon_values, epsilon_splits, data, candidate, race, elect_col, 
+def plot_elect_grid(epsilon_values, epsilon_splits, data, candidate, race, cand_perc_col, 
                     tot_vote, figsize=(10,10), filt=True, title=None, weight=False, n_samps=32):
     
     fig, axs = plt.subplots(len(epsilon_values),len(epsilon_splits), figsize=figsize)
@@ -62,7 +62,7 @@ def plot_elect_grid(epsilon_values, epsilon_splits, data, candidate, race, elect
 
     for i in range(len(epsilon_values)):
         for j in range(len(epsilon_splits)):
-            plot_er_graph(data, candidate, race, None, elect_col, tot_vote, 
+            plot_er_graph(data, candidate, race, None, cand_perc_col, tot_vote, 
                           epsilon_values[i], epsilon_splits[j], 
                           title=False, ax=axs[i,j], filt=filt, weight=weight, n_samps=n_samps)
 
@@ -81,21 +81,21 @@ def plot_elect_grid(epsilon_values, epsilon_splits, data, candidate, race, elect
 
 
 ## Get Point estimates of unnoised data.
-def point_estimates(data, cand, race, elect_col, tot_vote, eps, split, filt=True, weight=False):
+def point_estimates(data, cand, race, cand_perc_col, tot_vote, eps, split, filt=True, weight=False):
     
     df = data.query("epsilon == @eps & split == @split")
     df = df.query("`{}` > 10".format(tot_vote)) if filt else df
     num_precints = df.shape[0]
     xs = np.reshape(np.linspace(0, 1, 100), (100,1))
     perc_race = np.reshape(df["{}_pct".format(race)].fillna(0).values, (num_precints, 1))
-    perc_cand = df["{}{}".format(cand, elect_col)].fillna(0) 
+    perc_cand = df["{}".format(cand_perc_col)].fillna(0) 
     weights = df[tot_vote].values if weight else None
     line = LinearRegression().fit(perc_race, perc_cand, weights)
     
     return line.predict([[0],[1]])
 
 
-def plot_point_estimates(data, cand, race, elect, elect_col, tot_vote, eps, split,
+def plot_point_estimates(data, cand, race, elect, cand_perc_col, tot_vote, eps, split,
                          filt=True, n_samps=32, ax=None, title=True, x_lims=None, weight=False):
     
     df = data.query("epsilon == @eps & split == @split")
@@ -103,7 +103,7 @@ def plot_point_estimates(data, cand, race, elect, elect_col, tot_vote, eps, spli
     num_precints = df.shape[0]
     xs = np.reshape(np.linspace(0, 1, 100), (100,1))
     perc_race = np.reshape(df["{}_pct".format(race)].fillna(0).values, (num_precints, 1))
-    perc_cand = df["{}{}".format(cand,elect_col)].fillna(0) 
+    perc_cand = df["{}".format(cand_perc_col)].fillna(0) 
     weights = df[tot_vote].values if weight else None
     line = LinearRegression().fit(perc_race, perc_cand, weights)
 
@@ -139,7 +139,7 @@ def plot_point_estimates(data, cand, race, elect, elect_col, tot_vote, eps, spli
     ax.set_xlabel("support for {}".format(cand))
     return ax
 
-def plot_point_estimate_grid(epsilon_values, epsilon_splits, data, candidate, race, elect_col, 
+def plot_point_estimate_grid(epsilon_values, epsilon_splits, data, candidate, race, cand_perc_col, 
                     tot_vote, figsize=(10,10), filt=True, title=True, x_lims=None, weight=False, n_samps=32):
     
     fig, axs = plt.subplots(len(epsilon_values),len(epsilon_splits), figsize=figsize)
@@ -149,7 +149,7 @@ def plot_point_estimate_grid(epsilon_values, epsilon_splits, data, candidate, ra
 
     for i in range(len(epsilon_values)):
         for j in range(len(epsilon_splits)):
-            plot_point_estimates(data, candidate, race, None, elect_col, tot_vote, 
+            plot_point_estimates(data, candidate, race, None, cand_perc_col, tot_vote, 
                                  epsilon_values[i], epsilon_splits[j], weight=weight,
                                  title=False, ax=axs[i,j], filt=filt, x_lims=x_lims, n_samps=n_samps)
 
@@ -167,15 +167,15 @@ def plot_point_estimate_grid(epsilon_values, epsilon_splits, data, candidate, ra
     return fig, axs
 
 
-def plot_er_graph_gaussian_noise(data, cand, race, elect, tot_vote, sigma,
+def plot_er_graph_gaussian_noise(data, cand, race, elect, cand_perc_col, tot_vote, sigma,
                   filt=True, n_samps=32, ax=None, title=True, plot_cvap=False, weight=False):
     
     df = data.query("`{}` > 10".format(tot_vote)) if filt else data
     num_precints = df.shape[0]
     xs = np.reshape(np.linspace(0, 1, 100), (100,1))
     perc_race = np.reshape((df[race] / df["VAP"]).fillna(0).values, (num_precints, 1))
-    perc_cand = df["{}D_18R_Governor_pct".format(cand)].fillna(0)
-    weights = df["18R_Governor_D_tot"].values if weight else None
+    perc_cand = df["{}".format(cand_perc_col)].fillna(0)
+    weights = df[tot_vote].values if weight else None
     line = LinearRegression().fit(perc_race, perc_cand, weights)
 
 
@@ -195,21 +195,21 @@ def plot_er_graph_gaussian_noise(data, cand, race, elect, tot_vote, sigma,
             
         line_noised = LinearRegression().fit(perc_race_noised, perc_cand, weights)
         ms[i] = line_noised.coef_[0]
-        ax.scatter(perc_race_noised, perc_cand, marker='.', color="lightcoral", s=weights, alpha=0.5)
+        ax.scatter(perc_race_noised, perc_cand, marker='.', color="lightcoral", alpha=0.5, rasterized=True)
         ax.plot(xs, line_noised.predict(xs), '-', color="red")
     
-    ax.scatter(perc_race, perc_cand, marker='.', color="b", s=weights, alpha=0.5)
+    ax.scatter(perc_race, perc_cand, marker='.', color="b", alpha=0.5, rasterized=True)
     ax.plot(xs, line.predict(xs), '-', color="navy", 
              label="m: {}".format(round(line.coef_[0], 3)))
     ax.plot([], [], '-', color="r", 
              label="E(m): {}, Var(m): {}".format(round(np.mean(ms),3), round(np.var(ms),4)))  
     
-    ax.legend()
+    ax.legend(loc="lower right")
     ax.set_xlabel("% {}".format(race))
     ax.set_ylabel("{} % of Voters".format(cand))
     return ax
 
-def plot_elect_grid_gaussian_noise(epsilon_values, epsilon_splits, data, candidate, race, election, 
+def plot_elect_grid_gaussian_noise(epsilon_values, epsilon_splits, data, candidate, race, cand_perc_col,
                                    tot_vote, sigma_matches, allow_neg=False, figsize=(10,10), filt=True, 
                                    title=None, weight=False, n_samps=32):
     
@@ -223,7 +223,7 @@ def plot_elect_grid_gaussian_noise(epsilon_values, epsilon_splits, data, candida
             eps = epsilon_values[i]
             split = epsilon_splits[j]
             sigma = sigma_matches.query("allow_neg == @allow_neg and eps == @eps and split == @split").sigma
-            plot_er_graph_gaussian_noise(data, candidate, race, election, tot_vote, 
+            plot_er_graph_gaussian_noise(data, candidate, race, None, cand_perc_col, tot_vote, 
                                          sigma, title=False, ax=axs[i,j], filt=filt, weight=weight, n_samps=n_samps)
             axs[i,j].set_title("$\sigma$ = {}".format(round(float(sigma), 2)))
 
