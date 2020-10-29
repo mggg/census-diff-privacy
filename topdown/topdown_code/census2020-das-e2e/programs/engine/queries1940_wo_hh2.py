be2fa312a8bfc48fa35dc590417aed39ec3dece0
Bhushan Suwal <bhushan_suwal@live.com>
2020-10-29 18:24:28 -0400

    add topdown code used to run experiments

---
+++ topdown/topdown_code/census2020-das-e2e/programs/engine/queries1940_wo_hh2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import programs.engine.cenquery as cenquery
+
+
+class QueriesCreator1940():
+    """
+    1940 queries that correspond to those used in the 2018 e2e test.
+    """
+    def __init__(self, hist_shape, query_names):
+        self.query_names = query_names
+        self.hist_shape = hist_shape
+
+        self.query_funcs_dict = {
+            "va_hisp_race"               : self.va_hisp_race
+        }
+        self.queries_dict = {}
+
+    def calculateQueries(self):
+        for name in self.query_names:
+            assert name in self.query_names, "Provided query name '{}' not found.".format(name)
+            self.query_funcs_dict[name]()
+        return self
+
+    # 3 way marginals
+    def va_hisp_race(self):
+        add_over_margins = [0]
+        self.queries_dict["va_hisp_race"] = cenquery.Query(self.hist_shape, add_over_margins=add_over_margins, name="va_hisp_race")
