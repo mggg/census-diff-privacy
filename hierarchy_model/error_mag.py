014b93a7296f4ca096a208778a1327db6e48ab97
JN <jenniniels.matthews@gmail.com>
2021-03-25 18:00:49 -0400

    add small county code and ploting scripts

--- hierarchy_model/error_mag.py
+++ hierarchy_model/error_mag.py
@@ -51,36 +51,38 @@ fig.show()
 
 
 errors = pd.DataFrame()
-for alg in ["block_recom", "block_discon", "tract_discon"]:
-    errors = errors.append(pd.read_csv("{}_equal_split_errors.csv".format(alg)))
-    errors = errors.append(pd.read_csv("{}_bottom_heavy_split_errors.csv".format(alg)))
-    errors = errors.append(pd.read_csv("{}_mid_heavy_split_errors.csv".format(alg)))
-    errors = errors.append(pd.read_csv("{}_bg_heavy_split_errors.csv".format(alg)))
+for alg in  ["block_recom_175", "block_discon_175", "bg_recom_175", "bg_discon_175"]: #["tract_discon", "tract_recom_4", "block_recom", "block_discon"]:
+    if alg in ["tract_recom_4", "block_recom_175", "block_discon_175", "bg_recom_175", "bg_discon_175"]:
+        errors = errors.append(pd.read_csv("dist_errors/{}_equal_split_errors.csv".format(alg)).query("dist_id < 'tract_recom_4_400'"))
+        errors = errors.append(pd.read_csv("dist_errors/{}_bottom_heavy_split_errors.csv".format(alg)).query("dist_id < 'tract_recom_4_400'"))
+        errors = errors.append(pd.read_csv("dist_errors/{}_mid_heavy_split_errors.csv".format(alg)).query("dist_id < 'tract_recom_4_400'"))
+        errors = errors.append(pd.read_csv("dist_errors/{}_bg_heavy_split_errors.csv".format(alg)).query("dist_id < 'tract_recom_4_400'"))
+    else:
+        errors = errors.append(pd.read_csv("{}_equal_split_errors.csv".format(alg)))
+        errors = errors.append(pd.read_csv("{}_bottom_heavy_split_errors.csv".format(alg)))
+        errors = errors.append(pd.read_csv("{}_mid_heavy_split_errors.csv".format(alg)))
+        errors = errors.append(pd.read_csv("{}_bg_heavy_split_errors.csv".format(alg)))
+
+mean_error = errors.groupby(["dist_id", "model", "split", "district_type"]).mean().reset_index()
 
 
 eq_errors = pd.DataFrame()
+for alg in ["tract_recom", "tract_discon",
+            #"blockgroup_recom", "precinct_recom", "block_recom",
+           # "block_discon", "tract_discon", "block_bounding_box"]:
+            "block_recom", "block_discon"]:
+    eq_errors = eq_errors.append(pd.read_csv("dist_errors/{}_10_equal_split_errors.csv".format(alg)))
+    eq_errors = eq_errors.append(pd.read_csv("dist_errors/{}_20_equal_split_errors.csv".format(alg)))
 
-for alg in ["tract_recom", "blockgroup_recom", "precinct_recom", "block_recom",
-            "block_discon", "tract_discon", "block_bounding_box"]:
-    eq_errors = eq_errors.append(pd.read_csv("{}_equal_split_errors.csv".format(alg)))
+    if "block" in alg:
+        eq_errors = eq_errors.append(pd.read_csv("dist_errors/{}_175_equal_split_errors.csv".format(alg)))
+    else:
+        eq_errors = eq_errors.append(pd.read_csv("dist_errors/{}_4_equal_split_errors.csv".format(alg)))
 
-mean_error = errors.groupby(["dist_id", "model", "split", "district_type"]).mean().reset_index()
 mean_erroreq = eq_errors.groupby(["dist_id", "model", "split", "district_type"]).mean().reset_index()
 
-
-
-# recom_errors = pd.read_csv("recom_errors.csv").query("district_type == 'tract' or district_type == 'block'")
-# non_recom_errors = pd.read_csv("error_no_recom.csv").query("district_type == 'tract_discon' or district_type == 'block_discon'")
-# errors = recom_errors.append(non_recom_errors).replace({"district_type": {"tract": "tract_recom", "block": "block_recom"}})
-# errors = pd.read_csv("errors_error_mag.csv").replace({"district_type": {"tract": "tract_recom", "block": "block_recom"}})
-# errors["weight"] = errors["district_type"].apply(lambda t: 4/10 if t.endswith("recom") else 1)
-# mean_error = pd.read_csv("district_error_means.csv").replace({"district_type": {"tract": "tract_recom", "block": "block_recom"}})
-# mean_error["weight"] = mean_error["district_type"].apply(lambda t: 4/10 if t.endswith("recom") else 1)
-
-
-# mean_error = errors.groupby(["eps", "district_type", "model", "weight", "split", "frag_score"]).mean().reset_index()
 epsilon_splits = ["equal", "mid_heavy", "bg_heavy" ,"bottom_heavy"]
-models = ["ToyDown_allow_neg", "TopDown_no_HH_cons"]
+models = ["ToyDown_allow_neg", "ToyDown_non_neg", "TopDown_no_HH_cons"]
 
 fig, axs = plt.subplots(4,2, figsize=(15,20))
 for i, split in enumerate(epsilon_splits):
@@ -89,9 +91,10 @@ for i, split in enumerate(epsilon_splits):
         sns.histplot(data=df1,
                      legend=(i==0 and j==1), bins=25, hue="district_type", 
                      x="ErrorMag", log_scale=True, element="step", ax=axs[i,j],
-                     hue_order=["block_recom", "tract_discon", "block_discon"])
+                     hue_order=["block_recom", "tract_discon", "block_discon"]
+                     )
         axs[i,j].set_xlabel("error magnitude")
-        axs[i,j].set_xlim(10**-1, 10**5)
+        axs[i,j].set_xlim(10**1, 10**5)
         axs[i,j].get_yaxis().set_ticks([])
         axs[i,j].set_ylabel("")
 
@@ -108,16 +111,17 @@ for ax, col in zip(axs[0], ["Model: {}".format(m) for m in models]):
 fig.savefig("errormag_with_log_axis.png")
 fig.show()
 
-fig, axs = plt.subplots(4,2, figsize=(15,20))
+fig, axs = plt.subplots(4,3, figsize=(15,20))
 for i, split in enumerate(epsilon_splits):
     for j, model in enumerate(models):
         df1 = mean_error.query("split == @split and model==@model")
         sns.histplot(data=df1,
-                     legend=(i==0 and j==1), bins=25, hue="district_type", 
+                     legend=False, bins=25, hue="district_type", 
                      x="ErrorMag", log_scale=True, element="step", ax=axs[i,j],
-                     hue_order=["block_recom", "tract_discon", "block_discon"])
+                    hue_order=["block_recom_175", "block_discon_175", "bg_recom_175", "bg_discon_175"]
+                     )
         axs[i,j].set_xlabel("mean error magnitude")
-        axs[i,j].set_xlim(10**2 / 2.5, 10**5 / 9)
+        axs[i,j].set_xlim(10**1, 10**4/4)
         axs[i,j].get_yaxis().set_ticks([])
         axs[i,j].set_ylabel("")
 
@@ -131,41 +135,85 @@ for ax, col in zip(axs[0], ["Model: {}".format(m) for m in models]):
     ax.annotate(col, xy=(0.5, 1), xytext=(0, ax.xaxis.labelpad + pad*4),
                 xycoords='axes fraction', textcoords='offset points',
                 size='large', ha='center', va='baseline')
-fig.savefig("mean_errormag_with_log_axis.png")
+fig.savefig("mean_errormag_with_log_axis_175_toy_no_legend.png")
 fig.show()
 
 
-fig, axs = plt.subplots(6,2, figsize=(15,25))
+fig, axs = plt.subplots(12,1, figsize=(7, 25))
 
-algs = ["tract_discon", "tract_recom", "blockgroup_recom", "precinct_recom", "block_bounding_box", 
-        "block_recom",]
+algs = ["tract_recom_4", "tract_discon_4", "tract_recom_10", "tract_discon_10","tract_recom_20", 
+        "tract_discon_20",
+        "block_recom_10", "block_discon_10","block_recom_20", "block_discon_20","block_recom_175", 
+        "block_discon_175",]# "blockgroup_recom", "precinct_recom","block_recom",]
+
+
 for i, alg in enumerate(algs):
     df_d = mean_erroreq.query("split == 'equal' and district_type == @alg")
-    sns.histplot(data=df_d, x="ErrorMag", element="step", ax=axs[i,1], hue="model",
-                hue_order=models, legend=i==0, binwidth=75)
-    sns.histplot(data=df_d.query("model == 'ToyDown_allow_neg'"), x="frag_score", element="step", 
-                ax=axs[i,0], binwidth=25)
-    axs[i,1].set_xlim(100, 3000)
-    axs[i,0].set_xlim(70, 1000)
-    axs[i,1].set_ylim(0, 240)
-    axs[i,0].set_ylim(0, 410)
+    # df_d = eq_errors.query("split == 'equal' and district_type == @alg")
+    g = sns.histplot(data=df_d, x="ErrorMag", element="step", ax=axs[i], hue="model",
+                hue_order=["ToyDown_allow_neg","ToyDown_non_neg", "TopDown_no_HH_cons"], 
+                legend=i==0, binwidth=75)
+    # sns.histplot(data=df_d.query("model == 'ToyDown_allow_neg'"), x="frag_score", element="step", 
+    #             ax=axs[i,0], binwidth=25)
+    axs[i].set_xlim(-100, 2500)
+    # axs[i,0].set_xlim(70, 1000)
+    # axs[i,1].set_ylim(0, 240)
+    axs[i].set_ylim(0, 700)
     # [(axs[i,j].get_yaxis().set_ticks([]), axs[i,j].set_ylabel("")) for j in [0,1]]
+    # if i == 0:
+        # axs[i].legend(bbox_to_anchor=(1.01, 1),borderaxespad=0)
+
+
     
 
+
 pad = 5
-for ax, row in zip(axs[:,0], ["Alg = {}".format(s) for s in algs]):
+for ax, row in zip(axs, ["{}".format(s) for s in algs]):
     ax.annotate(row, xy=(0, 0.5), xytext=(-ax.yaxis.labelpad - pad, 0),
                 xycoords=ax.yaxis.label, textcoords='offset points',
                 size='large', ha='right', va='center')
-for ax, col in zip(axs[0], ["{}".format(m) for m in ["Frag Score", "Mean Error Magnitude"]]):
-    ax.annotate(col, xy=(0.5, 1), xytext=(0, ax.xaxis.labelpad + pad*4),
+# for ax, col in zip(axs[0], ["{}".format(m) for m in ["Frag Score", "Mean Error Magnitude"]]):
+#     ax.annotate(col, xy=(0.5, 1), xytext=(0, ax.xaxis.labelpad + pad*4),
+#                 xycoords='axes fraction', textcoords='offset points',
+#                 size='large', ha='center', va='baseline')
+axs[0].annotate("Mean Error Magnitude", xy=(0.5, 1), xytext=(0, ax.xaxis.labelpad + pad*4),
                 xycoords='axes fraction', textcoords='offset points',
                 size='large', ha='center', va='baseline')
 
-fig.savefig("mean_error_mag_and_frag_score_equal_split.png", bbox_inches='tight', dpi=200)
+
+fig.savefig("mean_error_mag_and_frag_score_equal_split_debug_with_small dist.png", bbox_inches='tight', dpi=200)
 
 
 
+algs = ["tract_recom_4",  "tract_recom_10", "tract_recom_20", 
+        "tract_discon_4","tract_discon_10", "tract_discon_20",
+        "block_recom_10", "block_recom_20", "block_recom_175", 
+        "block_discon_10","block_discon_20","block_discon_175"]
+
+df_d = mean_erroreq.query("split == 'equal'")
+g = sns.FacetGrid(df_d, col="district_type",hue="model", col_order=algs, col_wrap=3, xlim=(0,2500),
+                hue_order=["ToyDown_allow_neg","ToyDown_non_neg", "TopDown_no_HH_cons"], legend_out=True)
+g.map(sns.histplot, "ErrorMag", binwidth=75, alpha=0.5)
+g.set_axis_labels("Mean Error Magnitude (TOTPOP)", "Count")
+g.set_titles(col_template="{col_name}")
+g.add_legend()
+g.savefig("mean_error_mag_and_frag_score_equal_split_debug_with_small_dist.png", bbox_inches='tight', dpi=200)
 
 
-errors.query("district_type == 'tract_discon'")[["frag_score", "TOTPOP10G"]].drop_duplicates().shape
+np.load("sample_districts/1000_recom_tract_frags.npy").min()
+np.load("sample_districts/1000_recom_tract_frags.npy").max()
+
+np.load("sample_districts/1000_recom_block_frags.npy")
+
+pd.read_csv("tract_discon_equal_split_errors.csv").frag_score.min()
+df = pd.read_csv("tract_discon_equal_split_errors.csv").groupby(["dist_id", "model", "split", "district_type"]).mean().reset_index()
+
+df.query("model == 'ToyDown_allow_neg'").ErrorMag.min()
+df.query("model == 'ToyDown_allow_neg'").ErrorMag.max()
+
+df.query("model == 'TopDown_no_HH_cons'").ErrorMag.min()
+df.query("model == 'TopDown_no_HH_cons'").ErrorMag.max()
+
+plt.hist(df.query("model == 'ToyDown_allow_neg'").ErrorMag)
+
+plt.hist(df.query("model == 'TopDown_no_HH_cons'").ErrorMag)
---
+++ small_counties/district_errors.py
@@ -0,0 +1,102 @@
+# from gerrychain import (Graph, Partition, MarkovChain, tree,
+#                         proposals, updaters, constraints, accept, Election)
+# from gerrychain.updaters import Tally, cut_edges
+# from gerrychain.proposals import recom
+from treelib import Node, Tree
+import geopandas as gpd
+import numpy as np
+# import matplotlib.pyplot as plt
+import pickle
+from functools import partial
+import pandas as pd
+import seaborn as sns
+from tqdm import tqdm, trange
+# from local_tools.randomDistricts import Hierarchy_2D
+
+NUM_DISTS = 1000
+NUM_RUNS = 16
+
+SPLITS = {"equal": [0.2,0.2,0.2,0.2,0.2], 
+          "top_heavy": [1/2, 1/4, 1/12, 1/12, 1/12],
+          "mid_heavy": [1/12, 1/6, 1/2, 1/6, 1/12], 
+          "bottom_heavy": [1/12, 1/12, 1/12, 1/4, 1/2],
+          "bg_heavy": [1/12, 1/6, 1/6, 1/2, 1/12]}
+
+POPCOLS = ['HISP', 'NH_WHITE', 'NH_BLACK', 'NH_AMIN', 
+            'NH_ASIAN', 'NH_NHPI', 'NH_OTHER*']
+
+topdown_run_cols = ["Run_{}".format(i+1) for i in range(NUM_RUNS)]
+dist_blks = lambda d: [k for k,v in d.items() if v]
+
+def unique_dists(ds):
+    d_blks = np.array(list(map(dist_blks, ds)))
+    return np.unique(d_blks)[:NUM_DISTS]
+
+def dist_errors(alg, ds, blocks, frags=np.ones(NUM_DISTS)):
+    print("Starting {} calculations".format(alg), flush=True)
+    splits = ["equal", "mid_heavy", "bottom_heavy", "bg_heavy"]#, "top_heavy", "mid_heavy", "bottom_heavy", "bg_heavy"] #if (alg.endswith("discon") or alg == "block_recom") else []
+    d_blks = np.array(list(map(dist_blks, ds))) #unique_dists(ds)
+    for split in splits:
+        print("Split: {}".format(split))
+        toydown_allow_neg = pd.read_csv("../multi_attribute_toydown/results/small_cntys/ToyDown_runs_allow_neg_1_{}.csv".format(split))
+        toydown_non_neg = pd.read_csv("../multi_attribute_toydown/results/small_cntys/ToyDown_runs_non_neg_1_{}.csv".format(split))
+        toydown_allow_neg["TOTPOP"] = toydown_allow_neg[POPCOLS].sum(axis=1)
+        toydown_non_neg["TOTPOP"] = toydown_non_neg[POPCOLS].sum(axis=1)
+        toydown_allow_neg["GEOID"] = toydown_allow_neg["GEOID"].astype(str)
+        toydown_non_neg["GEOID"] = toydown_non_neg["GEOID"].astype(str)
+        toydown_allow_neg.set_index("GEOID", inplace=True)
+        toydown_non_neg.set_index("GEOID", inplace=True)
+
+        # topdown_no_hh = pd.read_csv("../tot_pops/TEXAS_STUB_{}_1_block_pops.csv".format(split))
+        # topdown_no_hh.GEOID10 = topdown_no_hh.GEOID10.astype(str)
+        # topdown_no_hh = pd.merge(blocks, topdown_no_hh, left_on="GEOID10", right_on="GEOID10", how="left").fillna(0)
+        # topdown_no_hh.set_index("GEOID10", inplace=True)
+
+        df = pd.DataFrame(columns=["dist_id", "frag_score", "TOTPOP10", "model", "Run", "TOTPOP_N"])
+
+        for i in tqdm(range(NUM_DISTS)):
+            d = d_blks[i]
+            district_type = alg
+            district_id = alg + "_{}".format(i)
+            # frag_score = h.assign_district_tree_variance(d, eps=1, sensitivity=1,
+            #                                              eps_splits=[np.sqrt(2)]*5)
+            frag_score = frags[i]
+            totpop = blocks.set_index("GEOID10").loc[d].TOTPOP.sum()
+            toydown_allow_neg_pops = toydown_allow_neg.loc[d].groupby("run").sum().TOTPOP.values
+            toydown_non_neg_pops = toydown_non_neg.loc[d].groupby("run").sum().TOTPOP.values
+            # topdown_pops = topdown_no_hh.loc[d][topdown_run_cols].sum(axis=0).values
+
+            models = [("ToyDown_allow_neg", toydown_allow_neg_pops), 
+                      ("ToyDown_non_neg", toydown_non_neg_pops), 
+                    #   ("TopDown_no_HH_cons", topdown_pops)
+                      ]
+            for j, (m_name, m_pops) in enumerate(models):
+                for k in range(NUM_RUNS):
+                    idx = i*len(models)*NUM_RUNS + j*NUM_RUNS + k
+                    df.loc[idx] = [district_id, frag_score, totpop, m_name, k, m_pops[k]]
+
+
+        df = df.assign(split=split, district_type=alg)
+        df = df.assign(Error=lambda r: r["TOTPOP_N"] - r["TOTPOP10"],
+                  ErrorMag=lambda r: np.abs(r["Error"]))
+        df.to_csv("dist_errors/{}_{}_split_errors.csv".format(alg, split), index=False)
+
+"""
+    Galveston City Block ReCom
+"""
+galv_city_blocks = gpd.read_file("shapes/blocks/galveston_city_blocks_2010_data.shp")
+block_recom = np.load("sample_districts/galveston_city_1000_recom_block_dists_6_eps_0.02.npy", allow_pickle=True)
+
+dist_errors("galveston_city_block_recom", block_recom, galv_city_blocks)
+
+"""
+    5 Counties (Bell, Brazoria, Cameron, Galveston, and Nueces) Tract ReCom/Discon
+"""
+counties = ["bell", "brazoria","cameron","galveston","nueces"]
+for county in counties:
+    blocks = gpd.read_file("shapes/blocks/{}_county_blocks_2010_data.shp".format(county))
+    tract_recom = np.load("sample_districts/{}_county_1000_recom_tract_dists_4_eps_0.02.npy".format(county), allow_pickle=True)
+    tract_discon = np.load("sample_districts/{}_county_1000_discon_tract_dists_4_eps_0.02.npy".format(county), allow_pickle=True)
+
+    dist_errors("{}_county_tract_recom".format(county), tract_recom, blocks)
+    dist_errors("{}_county_tract_discon".format(county), tract_discon, blocks)
